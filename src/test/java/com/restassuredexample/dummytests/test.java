c172a7899b903e2b8df9b00c62d60c4c63bf3518
udaykhare <udaykhare@gmail.com>
2020-07-19 20:51:29 +0530

    added Propertyloader file and logic

--- src/test/java/com/restassuredexample/dummytests/test.java
+++ src/test/java/com/restassuredexample/dummytests/test.java
@@ -1,18 +1,23 @@
-package com.restassuredexample;
+package com.restassuredexample.dummytests;
 
-
+import com.restassuredexample.util.*;
 import static io.restassured.RestAssured.*;
 import io.restassured.http.ContentType;
 import org.testng.annotations.*;
 import static org.hamcrest.Matchers.*;
 
 public class test {
+    String baseUrl;
+    @BeforeClass
+    public void setUp(){
+           baseUrl =propertyLoader.getUrl();
+    }
 
     @Test
     public void getEmployees(){
         given().
                 when().
-                get("http://dummy.restapiexample.com/api/v1/employees").
+                get(baseUrl+"/employees").
                 then().contentType(ContentType.JSON).statusCode(200);
 
     }
---
+++ src/test/java/com/restassuredexample/util/propertyLoader.java
@@ -0,0 +1,29 @@
+package com.restassuredexample.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+
+public class propertyLoader {
+    public static String url;
+    static {
+        Properties prop = new Properties();
+        try {
+            File file = new File("src/test/resources/config.properties");
+            InputStream fs = new FileInputStream(file);
+            prop.load(fs);
+            url = prop.getProperty("base.url");
+        }catch (IOException e){
+            System.exit(0);
+        }
+    }
+    public static String getUrl(){
+        return url;
+    }
+
+}
